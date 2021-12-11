fd7620a6a2fbbff411c53b052b65ada1ae900fe4
Chen, Po-Yen <poyenc@outlook.com>
2021-12-12 02:33:26 +0800

    Add WithIndex() for iterate elements with index

--- FlightConnection/Extensions.cs
+++ FlightConnection/Extensions.cs
@@ -63,5 +63,9 @@ namespace FlightConnection
         public static IEnumerable<T> Yield<T>(this T item) {
             yield return item;
         }
+
+        public static IEnumerable<(T item, int index)> WithIndex<T>(this IEnumerable<T> source) {
+            return source.Select((item, index) => (item, index));
+        }
     }
 }
